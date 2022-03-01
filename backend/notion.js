0e2735e7118fd80d6f7d1d65f3f56dd744253848
oliverrodin <79797556+oliverrodin@users.noreply.github.com>
2022-03-01 13:57:39 +0100

    init

---
+++ backend/notion.js
@@ -0,0 +1,13 @@
+const { Client } = require('@notionhq/client')
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY })
+
+async function getDatabase() {
+
+    const response = await notion.databases.retrieve({
+        database_id: process.env.NOTION_DATABASE_ID,
+    })
+    console.log(response)
+}
+
+getDatabase()
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+
+const express = require('express')
+const notion = require('./notion')
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hi")
+})
+
+app.listen(process.env.PORT)
